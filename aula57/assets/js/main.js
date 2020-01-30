14a9d02a5ac6e8e3fefac2f58d00377b352d20d9
William Firmino <williamwf07@gmail.com>
2020-01-30 17:28:01 -0300

    Criando Calculadora para Treinar Factory Functions

---
+++ aula57/assets/js/main.js
@@ -0,0 +1,79 @@
+function criaCalculadora() {
+    return {
+        display: document.querySelector('.display'),
+
+        inicia() {
+            this.cliqueBotoes();
+            this.pressionaEnter();
+        },
+
+        pressionaEnter() {
+            this.display.addEventListener('keyup', e => {
+                if (e.keyCode === 13) {
+                    this.realizaConta();
+                }
+            });
+        },
+
+        realizaConta() {
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta);
+
+                if(!conta) {
+                    alert('Conta Inválida!');
+                    return;
+                }
+
+                this.display.value = String(conta);
+            } catch(e) {
+                alert('Conta Inválida!');
+                return;
+            }
+        },
+        
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+        apagaUm() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+
+
+        cliqueBotoes() {
+            // this -> Calculadora
+            document.addEventListener('click', e => {
+                const el = e.target;
+
+                if(el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                if(el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
+
+                if(el.classList.contains('btn-del')) {
+                    this.apagaUm();
+                }
+
+                if(el.classList.contains('btn-eq')) {
+                    this.realizaConta();
+                }
+            });
+        },
+
+        btnParaDisplay(valor) {
+            this.display.value += valor;
+        }
+
+
+
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();

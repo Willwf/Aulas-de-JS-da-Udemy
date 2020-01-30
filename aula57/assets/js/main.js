ac36b542bebc198affe3980933457fc33ffa0c19
William Firmino <williamwf07@gmail.com>
2020-01-30 18:35:39 -0300

    Correção do Bug do Enter

--- aula57/assets/js/main.js
+++ aula57/assets/js/main.js
@@ -50,6 +50,8 @@ function criaCalculadora() {
 
                 if(el.classList.contains('btn-num')) {
                     this.btnParaDisplay(el.innerText);
+                    this.display.focus();
+
                 }
 
                 if(el.classList.contains('btn-clear')) {

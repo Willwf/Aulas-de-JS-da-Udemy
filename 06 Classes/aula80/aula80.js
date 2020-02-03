1cc389e4bf0402256c37ebb8b648610cd783dfde
William Firmino <williamwf07@gmail.com>
2020-02-03 16:55:55 -0300

    Criando Classes

--- 06 Classes/aula80/aula80.js
+++ 06 Classes/aula80/aula80.js
@@ -9,5 +9,14 @@ class Pessoa {
   }
 }
 
+function Pessoa2(nome, sobrenome) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+}
+
+Pessoa2.prototype.falar = function() {
+  console.log(`${this.nome} está falando.`);
+}
+
 const p1 = new Pessoa('Luiz', 'Miranda');
 console.log(p1);
